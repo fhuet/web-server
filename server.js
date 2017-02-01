12edf7932b265020898b25d6068394520b430d4a
Fred <fhuet@2xcel-us.com>
2017-02-01 14:44:45 -0800

    Refactor Middleware

--- server.js
+++ server.js
@@ -6,17 +6,7 @@ var PORT = 3000;
 //     res.send('Hello Express!');
 // });
 
-var middleware = {
-    requireAuthentication: function(req,res,next) {
-        console.log('Private route hit !');
-        next();
-    },
-    logger: function(req, res, next) {
-        // var reqDate = new Date().toString();
-        console.log('Request: ' + new Date().toString() + ':' + req.method + ' ' + req.originalUrl);
-        next();
-    }
-};
+var middleware = require('./middleware.js');
 
 app.use(middleware.logger);
 
