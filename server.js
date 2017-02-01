cdbec95f2d040f641e5384e7d4aaf2aae643a96c
Fred <fhuet@2xcel-us.com>
2017-02-01 14:19:02 -0800

    Initialize repo

---
+++ server.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+// app.get('/', function (req, res) {
+//     res.send('Hello Express!');
+// });
+
+var middleware = {
+    requireAuthentication: function(req,res,next) {
+        console.log('Private route hit !');
+        next();
+    },
+    logger: function(req, res, next) {
+        // var reqDate = new Date().toString();
+        console.log('Request: ' + new Date().toString() + ':' + req.method + ' ' + req.originalUrl);
+        next();
+    }
+};
+
+app.use(middleware.logger);
+
+//app.use(middleware.requireAuthentication);
+
+app.get('/About', middleware.requireAuthentication, function (req, res) {
+    res.send('Let\'s talk about us Baby!');
+});
+
+app.use(express.static(__dirname + '/public'));
+// console.log(__dirname);
+// / about
+// About Us
+
+app.listen(PORT, function () {
+    console.log('Express server started on port ' + PORT + '!');
+});
